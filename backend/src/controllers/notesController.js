1b1873eac9d6d8e6b3353aa0c16275c5d60d0fa3
Azmir <azmirmahni08@gmail.com>
2025-11-23 00:26:20 +0800

    create get and post method

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -10,8 +10,18 @@ export async function getAllNotes(req, res) {
   }
 }
 
-export function createNote(req, res) {
-  res.status(201).json({
-    message: "post created successfully",
-  });
+export async function createNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const note = new Note({ title, content });
+
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.error("Error in createNote controller ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
+export async function updateNote(req, res) {}
+export async function deleteNote(req, res) {}
