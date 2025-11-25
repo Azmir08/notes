cbb5fe660ad8d5cda26aa5e9072713cae3d48f57
Azmir <azmirmahni08@gmail.com>
2025-11-25 23:15:18 +0800

    create add note function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-
+// absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]
 function App() {
   return (
     <>
-      <div data-theme="forest">
+      <div data-theme="dracula">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/create" element={<CreatePage />} />
